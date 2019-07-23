55119c5cc7f21d63fd0408d3135b96b21b449bf7
Yue Dai <dy1983@gmail.com>
2019-07-23 08:06:36 +0800

    Update NYTChinese.py

--- books/NYTChinese.py
+++ books/NYTChinese.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from base import BaseFeedBook
+from base import BaseFeedBook, URLOpener, string_of_tag
 from lib.urlopener import URLOpener # 导入请求URL获取页面内容的模块
 from bs4 import BeautifulSoup # 导入BeautifulSoup处理模块
 
@@ -9,58 +9,23 @@ def getBook():
 
 class NYTChinese(BaseFeedBook):
     title                 = u'纽约时报中文网'
-    description           = u'纽约时报中文网'
+    description           = u'纽约时报中文网 国际纵览'
     language              = 'zh-cn'
     feed_encoding         = "utf-8"
     page_encoding         = "utf-8"
     mastheadfile          = "mh_nytchinese.gif"
     coverfile             = "cv_nytchinese.jpg"
-    # oldest_article        = 1
+    oldest_article        = 7
     
     feeds = [
-        # (u'首页', 'https://cn.nytimes.com/'),
-        (u'国际', 'https://cn.nytimes.com/world/'),
-        (u'中国', 'https://cn.nytimes.com/china/'),
-        (u'经济', 'https://cn.nytimes.com/business/'),
-        (u'镜头', 'https://cn.nytimes.com/lens/'),
-        (u'科技', 'https://cn.nytimes.com/technology/'),
-        (u'科学', 'https://cn.nytimes.com/science/'),
+        (u'国际纵览', 'https://cn.nytimes.com/rss/'),
     ]
     
-    fulltext_by_readability = False # 设定手动解析网页
+    # fulltext_by_readability = False
     
     keep_only_tags = [
         dict(name='div', class_='article-header'),
         dict(name='div', class_='article-paragraph'),
     ]
-
-    max_articles_per_feed = 3 # 设定每个主题下要最多可抓取的文章数量
-    # oldest_article = 3 # 设定文章的时间范围。小于等于365则单位为天，否则单位为秒，0为不限制。
-
-    def ParseFeedUrls(self):
-        urls = [] # 定义一个空的列表用来存放文章元组
-        # 循环处理fees中两个主题页面
-        for feed in self.feeds:
-            # 分别获取元组中主题的名称和链接
-            topic, url = feed[0], feed[1]
-            # 请求主题链接并获取相应内容
-            opener = URLOpener(self.host, timeout=self.timeout)
-            result = opener.open(url)
-            # 如果请求成功，并且页面内容不为空
-            if result.status_code == 200 and result.content:
-                # 将页面内容转换成BeatifulSoup对象
-                soup = BeautifulSoup(result.content, 'lxml')
-                # 找出当前页面文章列表中所有文章条目
-                items = soup.find_all(name='h3', class_='regularSummaryHeadline')
-                # 循环处理每个文章条目
-                for item in items:
-                    title = item.a.string # 获取文章标题
-                    link = item.a.get('href') # 获取文章链接
-                    link = BaseFeedBook.urljoin(url, link) # 合成文章链接
-                    urls.append((topic, title, link, None)) # 把文章元组加入列表
-            # 如果请求失败通知到日志输出中
-            else:
-                self.log.warn('Fetch article failed(%s):%s' % \
-                    (URLOpener.CodeMap(result.status_code), url))
-        # 返回提取到的所有文章列表
-        return urls
+    
+    max_articles_per_feed = 20 # 设定每个主题下要最多可抓取的文章数量
